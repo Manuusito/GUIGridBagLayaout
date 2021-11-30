47a958dd2f5a22bb4356452c51fee2ad958c423a
Manuel Cuellar <manuel.cuellar@correounivalle.edu.co>
2021-11-30 10:47:11 -0500

    Added class Dado

---
+++ src/Craps/Dado.java
@@ -0,0 +1,23 @@
+package Craps;
+
+import java.util.Random;
+
+/**
+ * Class Dado generate a Random value between 1 and 6
+ * @autor Manuel Cuellar
+ * @version v.1.0.0 date 30/11/2021
+ */
+public class Dado {
+    private int cara;
+
+    /**
+     * Method that generate a random value to cara
+     * @return number between (1,6)
+     */
+
+    public int getCara() {
+        Random aleatorio =new Random();
+        cara = aleatorio.nextInt(6)+1;
+        return cara;
+    }
+}
